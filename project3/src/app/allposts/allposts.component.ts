5b7c0555473f41d5fa632cca2ecd343543e527ec
Jared Hawley <j.yelwah@gmail.com>
2018-11-12 17:26:21 -0500

    Added components for what we need so far

---
+++ project3/src/app/allposts/allposts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-allposts',
+  templateUrl: './allposts.component.html',
+  styleUrls: ['./allposts.component.scss']
+})
+export class AllpostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
