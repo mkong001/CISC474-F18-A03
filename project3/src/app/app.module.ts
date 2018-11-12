80f50cddd53b684260cf03e344ac824d7404eed6
Jared Hawley <j.yelwah@gmail.com>
2018-11-12 17:25:56 -0500

    Added components for the pages we need so far

--- project3/src/app/app.module.ts
+++ project3/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllpostsComponent } from './allposts/allposts.component';
+import { MypostsComponent } from './myposts/myposts.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllpostsComponent,
+    MypostsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
