91030fd26e583c5c90d45c4cf528e64d110dec64
Minji Kong <mkong@udel.edu>
2018-11-15 10:32:12 -0500

    changed callbackURL

--- project3/src/app/auth/auth0-variables.ts
+++ project3/src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'lmVobpE1KJDTC1I1ab7LEbAx75dVqMN6',
   domain: 'cisctutor.auth0.com',
-  callbackURL: 'http://localhost:3000/callback'
+  callbackURL: 'http://localhost:4200/callback'
 };
